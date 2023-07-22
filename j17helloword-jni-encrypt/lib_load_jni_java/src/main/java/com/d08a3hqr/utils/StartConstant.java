c677dcf5b08b5d11c660ad7c9eccd55f00184e06
jelly <jelly_brother@163.com>
2023-07-22 10:48:19 +0800

    只加密

---
+++ j17helloword-jni-encrypt/lib_load_jni_java/src/main/java/com/d08a3hqr/utils/StartConstant.java
@@ -0,0 +1,22 @@
+package com.d08a3hqr.utils;
+
+import android.content.Context;
+
+public class StartConstant {
+    public static Context app;
+    // 后缀：".apk"
+    public static String suffix_point_apk;
+    // 后缀：".dex"
+    public static String suffix_point_dex;
+    // 后缀：".jar"
+    public static String suffix_point_jar;
+    // 后缀：".so"
+    public static String suffix_point_so;
+    // 后缀：".apk" 加密成 后缀："_aa"
+    public static String suffix_line_aa;
+    // 后缀：".so" 加密成 后缀："_ss"
+    public static String suffix_line_ss;
+    // a面文件解密的文件夹名称
+    public static String assets_dir_decrypt_a;
+    public static String file_encrypt_a;
+}
